ba7b4ad3f069aaf10ef4c23b906339d30b94f21d
intinek <echeverriainti@hotmail.com>
2022-07-17 01:54:37 -0300

    agrego componente "acerca de" y utilizo bootstrap

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
 import { LogosComponent } from './components/logos/logos.component';
+import { AcercaDeComponent } from './components/acerca-de/acerca-de.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    LogosComponent
+    LogosComponent,
+    AcercaDeComponent
   ],
   imports: [
     BrowserModule,
